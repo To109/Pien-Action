5ea70c7ea80bb91a39a8194ff06beb8909860258
fewlio <seraja0824ryoiki@gmail.com>
2025-07-22 13:53:58 +0900

    ライフアイテムと時間アイテムのクラス作成

--- Assets/Scenes/script/Play/Item/LifeRecoveryItem.cs
+++ Assets/Scenes/script/Play/Item/LifeRecoveryItem.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 
-public class LifeRecoveryItem
+public class LifeRecoveryItem : ItemBase // ItemBaseを継承
 {
-    
+    [SerializeField]
+    private int healAmount = 1; // 回復量
+
+    // ItemBaseのApplyEffectメソッドの具体的な中身を記述
+    protected override void ApplyEffect(Player player)
+    {
+        // プレイヤーのHealメソッドを呼び出す
+        //player.Heal(healAmount);
+    }
 }
